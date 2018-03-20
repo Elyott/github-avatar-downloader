c2d1e83da1c89cb8ee490d525dadf54a8ffc9065
Elyott Ryan <elyott_ryan@hotmail.com>
2018-03-20 01:17:50 +0000

    connected the functions and it now downloads all the avatar_url pictures

--- download_avatars.js
+++ download_avatars.js
@@ -1,48 +1,46 @@
 var request = require('request');
 var fs = require('fs');
 var GitHubToken = require('./secrets.js')
-console.log(GitHubToken)
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+          .on('error', function(err){
+          console.log(err);
+          throw err;
+          })
+          .on('response', function(response){
+          console.log('Downloading image...');
+          })
+          .pipe(fs.createWriteStream(filePath))
+          .on('finish', function() {
+            console.log("Download complete.");
+          });
+}
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'GitHubToken'
+      'Authorization': GitHubToken.GITHUB_TOKEN
     },
     json: true
-  }
+  };
 
   request(options, function(err, res, body) {
-    var data = body;
-    data.forEach(function(contributor){
-      console.log(contributor.avatar_url);
+    body.forEach(function(contributor){
+      var filepath = "avatars/" + contributor.login + ".jpg";
+      downloadImageByURL(contributor.avatar_url, filepath);
     });
     cb(err, body);
   });
 
-
-
 };
-  function downloadImageByURL(url, filePath) {
-    request.get(url)
-            .on('error', function(err){
-            console.log(err);
-            throw err;
-            })
-            .on('response', function(response){
-            console.log('Downloading image...');
-            })
-            .pipe(fs.createWriteStream(filePath))
-            .on('finish', function() {
-              console.log("Download complete.");
-            });
-  }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  // console.log("Result:", result);
+  console.log("Result:", result);
 });
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
