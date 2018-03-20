feb1f5010b4abc9c1e76852c09c72a7b79a4b506
Elyott Ryan <elyott_ryan@hotmail.com>
2018-03-20 00:29:02 +0000

    update request function to print out avatar_url for each contributor

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,31 @@
 var request = require('request');
-
+var GitHubToken = require('./secrets.js')
+console.log(GitHubToken)
 console.log('Welcome to the GitHub Avatar Downloader!');
-
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
-    }
+      'User-Agent': 'request',
+      'Authorization': 'GitHubToken'
+    },
+    json: true
   }
 
   request(options, function(err, res, body) {
+    var data = body;
+    data.forEach(function(contributor){
+      console.log(contributor.avatar_url);
+    });
     cb(err, body);
   });
+
+
+
 };
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
 });
